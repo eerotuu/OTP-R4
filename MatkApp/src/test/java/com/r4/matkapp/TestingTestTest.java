4699470a4ebddc7a8b3f9b55bbf1a6978baff99c
eerotuu <eerotuu@metropolia.fi>
2019-02-11 12:08:14 +0200

    added required libraries and framework description to readme

--- MatkApp/src/test/java/com/r4/matkapp/TestingTestTest.java
+++ MatkApp/src/test/java/com/r4/matkapp/TestingTestTest.java
@@ -42,13 +42,7 @@ public class TestingTestTest {
      */
     @org.junit.Test
     public void testSum() {
-        System.out.println("sum");
-        TestingTest instance = null;
-        int expResult = 0;
-        int result = instance.sum();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
     
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OTP-R4
+
+## Frameworks and libraries
+	- Hibernate 5.2.17
+	- mysql-connector-java 5.1.47
+	- hamcroest-core 1.3
+	- junit 4.12
