b0bc895fc3d65d231c5122c50490f6dcc001e665
eerotuu <eerotuu@metropolia.fi>
2019-02-11 11:58:02 +0200

    added junit, msql-connector and hibernate depencies

---
+++ MatkApp/src/main/java/com/r4/matkapp/TestingTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.r4.matkapp;
+
+/**
+ *
+ * @author Eero
+ */
+public class TestingTest {
+    
+    int val1, val2;
+
+    public TestingTest(int val1, int val2) {
+        this.val1 = val1;
+        this.val2 = val2;
+    }
+    
+    public int sum() {
+        return val1 + val2;
+    }
+}
