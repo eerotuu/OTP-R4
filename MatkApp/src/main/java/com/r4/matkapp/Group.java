a19f33f9555166ed4aa6845e2ed2098cac57b498
Heikki Sillanpaa <heikksil@metropolia.fi>
2019-02-14 12:10:34 +0000

    Group.java muokattu

--- MatkApp/src/main/java/com/r4/matkapp/Group.java
+++ MatkApp/src/main/java/com/r4/matkapp/Group.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.r4.matkapp;
 
-/**
- *
- * @author silla
- */
 public class Group {
+    private int group_id;
+    private String group_name;
+
+    public Group(String group_name) {
+        this.group_name = group_name;
+    }
+
+    public int getGroup_id() {
+        return group_id;
+    }
+
+    public String getGroup_name() {
+        return group_name;
+    }
+
+    public void setGroup_name(String group_name) {
+        this.group_name = group_name;
+    }
     
 }
