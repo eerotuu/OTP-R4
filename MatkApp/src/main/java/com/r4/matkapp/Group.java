8646d3c9d65abc076a26534048bbac41f2088bb3
Mika Iso-Ikala <mika.iso-ikala@metropolia.fi>
2019-02-18 13:34:01 +0200

    Muokattiin Group-luokkaan JPE määreet

--- MatkApp/src/main/java/com/r4/matkapp/Group.java
+++ MatkApp/src/main/java/com/r4/matkapp/Group.java
@@ -1,8 +1,22 @@
 package com.r4.matkapp;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "group")
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id", updatable = false, nullable = false)
     private int group_id;
+    
+    @Column(name="group_name")
     private String group_name;
+    
+    public Group() {
+        super();
+    }
 
     public Group(String group_name) {
         this.group_name = group_name;
@@ -19,5 +33,5 @@ public class Group {
     public void setGroup_name(String group_name) {
         this.group_name = group_name;
     }
-    
+
 }
