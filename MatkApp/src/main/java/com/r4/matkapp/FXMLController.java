3bb819584d220a1ff2551f637f82ebac4fd76c07
Heikki <Heikki@DESKTOP-KG5GLLE.wlan>
2019-03-07 13:18:44 +0200

    User interface started. FXMLController & Scene.fxml in progress. Added basic navigation to the user interface.

--- MatkApp/src/main/java/com/r4/matkapp/FXMLController.java
+++ MatkApp/src/main/java/com/r4/matkapp/FXMLController.java
@@ -5,17 +5,31 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
 public class FXMLController implements Initializable {
     
     @FXML
-    private Label label;
+    private Button users_button, groups_button, mainpage_button, mainpage2_button;
+    
+    @FXML
+    private StackPane users_pane, groups_pane, mainpage_pane;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        if (event.getSource() == users_button) {
+            users_pane.toFront();
+        }
+        else if (event.getSource() == groups_button) {
+            groups_pane.toFront();
+        }
+        else if (event.getSource() == mainpage_button) {
+            mainpage_pane.toFront();
+        }
+        else if (event.getSource() == mainpage2_button) {
+            mainpage_pane.toFront();
+        }
     }
     
     @Override
