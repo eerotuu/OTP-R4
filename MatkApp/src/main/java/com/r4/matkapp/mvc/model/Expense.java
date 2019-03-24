4ed64861ea768a5247abcf42b9d399ba8273ac6d
Mika Iso-Ikala <mika.iso-ikala@metropolia.fi>
2019-03-24 17:24:12 +0200

    Expense-luokan määrittelyä

--- MatkApp/src/main/java/com/r4/matkapp/mvc/model/Expense.java
+++ MatkApp/src/main/java/com/r4/matkapp/mvc/model/Expense.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.r4.matkapp.mvc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  *
- * @author Eero
+ * @author Eero & Mika
  */
 @Entity
 @Table(name = "expense")
@@ -25,8 +15,27 @@ public class Expense {
     @Column(name = "id", updatable = false, nullable = false)
     private int id;
     
+    //Olettaen, että rakkenne kulut liitetään aina johonkin ryhmään
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_group")
+    private Group user_group;
+    @Column(name = "expense_description")
+    private String expense_description;
+    @Column(name = "expense_amount")
+    private double expense_amount;
+    /** ei nykysuunnitelmalla tarpeellinen kenttä
+     * @Column(name = "expense_currency")
+     * private String expense_currency
+     */
+    
+    //Ajattelumalli:
+    //True = kaikki osallistuu, tasajako
+    //False = ei kaikki osallistu tai ei tasajako
+    @Column(name = "equal_split")
+    private boolean equal_split;
+    
     public Expense() {
-        
+        super();
     }
 
     public int getId() {
@@ -36,4 +45,39 @@ public class Expense {
     public void setId(int id) {
         this.id = id;
     }
+
+    public Group getUser_group() {
+        return user_group;
+    }
+
+    public void setUser_group(Group user_group) {
+        this.user_group = user_group;
+    }
+
+    public String getExpense_description() {
+        return expense_description;
+    }
+
+    public void setExpense_description(String expense_description) {
+        this.expense_description = expense_description;
+    }
+
+    public double getExpense_amount() {
+        return expense_amount;
+    }
+
+    public void setExpense_amount(double expense_amount) {
+        this.expense_amount = expense_amount;
+    }
+
+    public boolean isEqual_split() {
+        return equal_split;
+    }
+
+    public void setEqual_split(boolean equal_split) {
+        this.equal_split = equal_split;
+    }
+    
+    
+    
 }
