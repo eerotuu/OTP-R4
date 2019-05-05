e8acc1aa00783ebf64bc1810fd7df17804fece6a
Mika Iso-Ikala <mika.iso-ikala@metropolia.fi>
2019-05-05 17:27:21 +0300

    Group Expenses: Expanded localisation Updated localisation files

--- MatkApp/src/main/java/com/r4/matkapp/mvc/controller/ExpensesListController.java
+++ MatkApp/src/main/java/com/r4/matkapp/mvc/controller/ExpensesListController.java
@@ -38,16 +38,16 @@ public class ExpensesListController implements Initializable {
     @FXML
     private VBox list, listJoined, listNotJoined;
     @FXML
-    private Label descriptionLabel, totalpriceLabel, participantsLabel, totalPrice;
+    private Label descriptionLabel, totalpriceLabel, participantsLabel, totalPrice, descriptionLabel1, totalpriceLabel1, participantsLabel1, totalLabel, descriptionLabel2, totalpriceLabel2, participantsLabel2;
     @FXML
-    private Button updateButton;
+    private Button updateButton, updateButton1, updateButton2;
     @FXML
-    Tab allExpenses, myExpenses;
+    private Tab allExpenses, myExpenses, notJoinedExpenses;
 
     private Group activeGroup;
     private AltGroupSceneController cont;
     private ResourceBundle bundle;
-    
+
     public ExpensesListController(AltGroupSceneController cont) {
         this.cont = cont;
     }
@@ -59,52 +59,61 @@ public class ExpensesListController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         bundle = ResourceBundle.getBundle("properties.default", MainApp.getLocale());
         refreshExpenseList();
-        
+
         descriptionLabel.setText(bundle.getString("GroupExpensesDescLabel"));
+        descriptionLabel1.setText(bundle.getString("GroupExpensesDescLabel"));
+        descriptionLabel2.setText(bundle.getString("GroupExpensesDescLabel"));
         totalpriceLabel.setText(bundle.getString("GroupExpensesPriceLabel"));
+        totalpriceLabel1.setText(bundle.getString("GroupExpensesMyPortionLabel"));
+        totalpriceLabel2.setText(bundle.getString("GroupExpensesPriceLabel"));
         participantsLabel.setText(bundle.getString("GroupExpensesParticipantLabel"));
+        participantsLabel1.setText(bundle.getString("GroupExpensesParticipantLabel"));
+        participantsLabel2.setText(bundle.getString("GroupExpensesParticipantLabel"));
         updateButton.setText(bundle.getString("GroupExpensesUpdateButton"));
+        updateButton1.setText(bundle.getString("GroupExpensesUpdateButton"));
+        updateButton2.setText(bundle.getString("GroupExpensesUpdateButton"));
         allExpenses.setText(bundle.getString("GroupExpensesTabAll"));
         myExpenses.setText(bundle.getString("GroupExpensesTabMine"));
+        notJoinedExpenses.setText(bundle.getString("GroupExpensesTabOthers"));
+        totalLabel.setText(bundle.getString("GroupExpensesTotalPortionLabel") + " ");
     }
-    
+
     @FXML
-    public void refreshExpenseList() { 
+    public void refreshExpenseList() {
         cont.updateGroupData();
-    } 
-    
+    }
+
     public void loadExpenseList() {
-            
+
         list.getChildren().remove(1);
         list.getChildren().add(new ExpenseListFactory(this).createList(cont.getSelectedGroup().getExpenses()));
-        
+
         Set<Expense>[] expenses = split(cont.getSelectedGroup().getExpenses());
         listNotJoined.getChildren().remove(1); // remove old list (index 1)
         listNotJoined.getChildren().add(new ExpenseListFactory(this).createList(expenses[0]));
-        
+
         listJoined.getChildren().remove(1);
         listJoined.getChildren().add(new ExpenseListFactory(true).createList(expenses[1]));
         double total = 0;
-        for(Expense e : expenses[1]) {
+        for (Expense e : expenses[1]) {
             total += e.getExpense_amount();
         }
         totalPrice.setText(Double.toString(total));
-            
+
     }
-    
-    
-    private Set<Expense>[] split (Set<Expense> expenses) {
+
+    private Set<Expense>[] split(Set<Expense> expenses) {
         List<Expense> notJoined = new ArrayList<>();
         List<Expense> joined = new ArrayList<>();
         User loggedUser = DatabaseManager.getLoggedUser();
-        for(Expense e: expenses) {
+        for (Expense e : expenses) {
             boolean isJoined = false;
-            for(User u: e.getUsers()) {
-                if(loggedUser.getId() == u.getId()) {
+            for (User u : e.getUsers()) {
+                if (loggedUser.getId() == u.getId()) {
                     isJoined = true;
                 }
             }
-            if(isJoined) {
+            if (isJoined) {
                 joined.add(e);
             } else {
                 notJoined.add(e);
